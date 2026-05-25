434b78fae4741a8427b7a2dfb8a5603acbb1c1f5
ArthurFernandes06 <artrdgs@gmail.com>
2026-05-24 21:23:37 -0300

    Atualizando as dependencias no package.json e subindo um main com rotas

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import App from './pages/home/App.jsx'
+import Login from './pages/login/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )

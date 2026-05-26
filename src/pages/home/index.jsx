c2c66bc14b1bea959b3b6f37d248bfc6511e5251
Gabriel Teixeira <gabrieldamasqueiroz@gmail.com>
2026-05-25 23:38:38 -0300

    feat: adicionando tela da homepage.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,56 @@
 import React from "react";
 import "./style.css"
+import GitHubIcon from "../../assets/github.svg"
+import Profile from "../../assets/profile.svg"
 
 export default function Home() {
     return (
 
-        <div>
-            <h1>home</h1>
+        <div className="page">
+
+            <header>
+                <div id="header-container">
+
+                    <div id="title-and-icon">
+                        <a href="https://github.com/Gabriel-Tex/todo-list-ceos-frontend" target="_blank">
+                            <img src={GitHubIcon} alt="github icon" />
+                        </a>
+                        <h1>TODO List</h1>
+                    </div>
+
+                    <a href="#">
+                        <img src={Profile} alt="profile icon" />
+                    </a>
+                </div>
+
+                <input className="search" type="text" placeholder="Pesquisar"/>
+            </header>
+
+            <main>
+                <div id="task-list">
+                    <form className="add-task" action="#">
+                       { /*<input id="input-task" type="text" placeholder="Adicionar tarefa"/> */ }
+                        <button>Adicionar tarefa</button>
+                    </form>
+                    
+
+                    <div className="task">
+                        <p>task</p>
+                    </div>
+
+                    <div className="task">
+                        <p>task</p>
+                    </div>
+
+                    <div className="task">
+                        <p>task</p>
+                    </div>
+                </div>
+            </main>
+
+            <footer>
+                <p>TODO List | PS da CEOS</p>
+            </footer>
         </div>
     );
 }
